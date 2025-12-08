bfa54bb112a63b1ec9211814322005c6c2b9f00f
Sebastian <sebastianszetela8@gmail.com>
2025-12-08 10:50:19 +0200

    Update tasks.py

--- RPA-PROJECT/tasks.py
+++ RPA-PROJECT/tasks.py
@@ -1,5 +1,6 @@
 from robocorp.tasks import task
 from robocorp import windows
+<<<<<<< Updated upstream
 from RPA.Desktop.Windows import Desktop
 desktop = windows.desktop()
 desktop2=Desktop()
@@ -31,10 +32,50 @@ def open_ltspice():
     desktop.send_keys("{Ctrl}c")#copy the text
 
 
+=======
+import time
+import os
+from robocorp.tasks import task
+from RPA.Desktop.Windows import Desktop
+desktop2=Desktop()
+circuit = r"C:\Users\USER\Documents\LTspice\pmos-switch-circuit-A.asc"
+desktop = windows.Desktop()
+@task
+def main():
+    desktop.windows_run(r"C:\Users\USER\AppData\Local\Programs\ADI\LTspice\LTspice.exe")
+    open_circuit(circuit)
+    run_simulation()
+    time.sleep(2)
+    screenshot()
+    copylog()
+    extract_voltage()
+def open_circuit(circuit):
+    lt = windows.find_window("name:LTspice", search_depth=1)
+    lt.send_keys('{Ctrl}o')
+    dlg = windows.find_window("regex:.*Open.*", search_depth=2)
+    dlg.send_keys( circuit )
+    time.sleep(0.1)
+    dlg.send_keys('{Enter}')
+def run_simulation():
+    lt = windows.find_window("name:LTspice - [pmos-switch-circuit-A.asc]", search_depth=1)
+    lt.send_keys('{ALT}{R}')
+    time.sleep(3)
+    lt.send_keys('{CTRL}{L}')
+def screenshot():
+    lt = windows.find_window("name:LTspice - pmos-switch-circuit-A.asc", search_depth=1)
+    lt.screenshot("screenshot.png")
+def copylog():
+    log_win = windows.find_window("regex:.*SPICE Output Log.*", search_depth=2)
+    log_win.send_keys('{Ctrl}a')
+    time.sleep(0.1)
+    log_win.send_keys('{Ctrl}c')
+    time.sleep(0.1)
+>>>>>>> Stashed changes
 
 def extract_voltage():
     desktop = windows.Desktop()
     raw_text=desktop2.get_clipboard_value()
+<<<<<<< Updated upstream
     row=raw_text.split("\n")[16]
     print(row)
     voltage_string=row.split(" ")[1]
@@ -48,3 +89,11 @@ def extract_voltage():
 
 
 
+=======
+    raw_text.split("\n")
+    line=raw_text.split("\t")
+    voltage=line[3]
+    desktop.windows_run('notepad.exe')
+    note = windows.find_window("regex:.*Notepad", search_depth=3)
+    note.send_keys(voltage)
+>>>>>>> Stashed changes
